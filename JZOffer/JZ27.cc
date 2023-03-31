15ea03b6d8427fe6603a0b93701b750f15d7ae4e
lutianen <tianenlu957@gmail.com>
2023-03-31 10:33:24 +0800

    JZ27 二叉树的镜像 添加“非递归实现” - 迭代 + 辅助栈

--- JZOffer/JZ27.cc
+++ JZOffer/JZ27.cc
@@ -4,9 +4,15 @@
 
     思路：递归 + 先序遍历
         交换左右节点即可
+
+    思路2：利用 栈 或 队列
  */
 #include <cstdio>
 #include <algorithm>
+#include <stack>
+
+using std::stack;
+using std::swap;
 
 // Definition for a binary tree node.
 struct TreeNode {
@@ -18,6 +24,12 @@ struct TreeNode {
 
 class Solution {
 public:
+    /**
+     * @brief 递归实现
+     *  可能会出现栈溢出的情况 
+     * @param root 
+     * @return TreeNode* 
+     */
     TreeNode* mirrorTree(TreeNode* root) {
         if (root == nullptr)
             return root;
@@ -28,6 +40,32 @@ public:
         // 递归 左子树、右子树
         mirrorTree(root->left);
         mirrorTree(root->right);
+
+        return root;
+    }
+
+    /**
+     * @brief 非递归实现 - 辅助栈
+     * 
+     * @param root 
+     * @return TreeNode* 
+     */
+    TreeNode* mirrorTree2(TreeNode* root) {
+        if (root == nullptr)
+            return root;
+
+        stack<TreeNode*> stk;
+        stk.push(root);
+        
+        while(!stk.empty()) {
+            TreeNode* curr = stk.top();
+            stk.pop();
+
+            if (curr->left != nullptr) stk.push(curr->left);
+            if (curr->right != nullptr) stk.push(curr->right);
+
+            swap(curr->left, curr->right);
+        }
 
         return root;
     }
