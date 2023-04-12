05a0456f1901bc09c5a9dd817a9cea9414b5a8c0
lutianen <tianenlu957@gmail.com>
2023-04-12 22:49:15 +0800

    JZ05 替换空格 思路：         1. stringstream         2. 原地替换 - 从后向前开始替换，利用双指针（p1 指向原子原字符串的最后，p2 指向字符数组的最后），             非空格的情况下，复制 p1 至 p2；空格的情况下，p1 前移，p2 填入 %20

--- JZOffer/JZ05.cc
+++ JZOffer/JZ05.cc
@@ -1,31 +1,31 @@
 /**
- * @file JZ05.cc
- * @brief 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * @file JZ05 替换空格
+ * @brief 请实现一个函数，把字符串 s 中的每个空格替换成"%20"
+
+    思路：
+        1. stringstream
+        2. 原地替换 - 从后向前开始替换，利用双指针（p1 指向原子原字符串的最后，p2 指向字符数组的最后），
+            非空格的情况下，复制 p1 至 p2；空格的情况下，p1 前移，p2 填入 %20
  */
 
-
 #include <iostream>
+#include <sstream>  // stringstream
 #include <string>
 
-#include <sstream>  // stringstream
-
+using std::cin;
+using std::cout;
+using std::endl;
 using std::string;
 using std::stringstream;
 
 class Solution {
 public:
-
     /**
      * @brief 利用 stringstream 将 s 中的字符根据是否为 ' '，依次放入ss中; 非原地替换
-     * 
-     * @param s 
-     * @return string 
      */
     string replaceSpace(string s) {
-        if (s.size() < 0) {
-            return {};
-        }
-        
+        if (s.size() < 0) return {};
+
         int length = s.size();
         stringstream ss;
 
@@ -35,7 +35,7 @@ public:
                 ss << "%20";
             } else {
                 ss << c;
-            } 
+            }
         }
         return ss.str();
     }
@@ -44,22 +44,21 @@ public:
      * @brief 原地替换：从后向前开始替换，利用双指针（p1 指向原子原字符串的最后，p2 指向字符数组的最后），
      * 非空格的情况下，复制 p1 至 p2；空格的情况下，p1 前移，p2 填入 %20
      * @note s.resize(s.size() + numSpace * 2)
-     * @param s 
-     * @return string 
+     * @param s
+     * @return string
      */
     string replaceBlink(string s) {
-        if (s.size() < 1)
-            return {};
+        if (s.size() < 1) return {};
         int p1 = s.size();
 
         int numSpace = 0;
-        for(int i = 0; i < s.size(); ++i) 
-            numSpace += (s[i] == ' ') ? 1 : 0;
+        for (int i = 0; i < s.size(); ++i) numSpace += (s[i] == ' ') ? 1 : 0;
 
         s.resize(s.size() + numSpace * 2);
 
-        for(int i = p1 - 1, j = s.size() - 1; i < j; i--, j--) {
-            if(s[i] != ' ') {
+        // 当 i == j 时，表示左侧已没有空格，跳出循环
+        for (int i = p1 - 1, j = s.size() - 1; i < j; i--, j--) {
+            if (s[i] != ' ') {
                 s[j] = s[i];
             } else {
                 s[j--] = '0';
@@ -68,16 +67,18 @@ public:
             }
         }
 
-        return s; 
+        return s;
     }
 };
 
 int main() {
-    Solution s;
-    // string ret = s.replaceSpace("string s  adas  a     d");
-    string ret = s.replaceBlink("We are happy.");
+    string str;
+    // 不能使用 cin
+    std::getline(cin, str);
 
-    std::cout << ret << std::endl;
+    Solution s;
+    cout << s.replaceSpace(str) << endl;
+    cout << s.replaceBlink(str) << endl;
 
     return 0;
 }
