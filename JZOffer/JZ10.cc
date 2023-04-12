583d17c7a7432dc30df0828277ec4cab715647d3
lutianen <tianenlu957@gmail.com>
2023-04-12 23:50:27 +0800

    JZ10 - I. 斐波那契数列 思路：DP / 滚动数组

--- JZOffer/JZ10.cc
+++ JZOffer/JZ10.cc
@@ -1,38 +1,57 @@
 /**
- * @brief 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
- F(0) = 0,   F(1) = 1  
- F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
- 
- 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
- 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * @brief JZ10 - I. 斐波那契数列
+        F(0) = 0,   F(1) = 1
+        F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+        答案需要取模 1e9+7
+
+    思路：DP / 滚动数组
  */
 
 #include <iostream>
 
 class Solution {
+    const static int MOD = 1e9 + 7;
+
 public:
     int fib(int n) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+        if (n < 2) return n;
 
-        unsigned long long ans[n+1];
+        unsigned long long ans[n + 1];
         ans[0] = 0;
         ans[1] = 1;
 
         for (int i = 2; i <= n; ++i) {
             ans[i] = ans[i - 1] + ans[i - 2];
-            ans[i] %= 1000000007;
+            ans[i] %= MOD;
         }
 
         return ans[n];
+    }
+
+    // 滚动数组
+    int fib2(int n) {
+        if (n < 2) return n;
+
+        int ans[2];
+        ans[0] = 0;
+        ans[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            int temp = ans[1] + ans[0];
+            ans[0] = ans[1];
+            ans[1] = temp % MOD;
+        }
+
+        return ans[1];
     }
 };
 
 int main() {
     Solution s;
-    int ret = s.fib(95);
 
-    std::cout << ret << std::endl;
+    int num;
+    std::cin >> num;
+
+    std::cout << s.fib(num) << std::endl;
+    std::cout << s.fib2(num) << std::endl;
 }
