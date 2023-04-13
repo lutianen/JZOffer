10190cdd09e77f951145c5183b2cf66ddf2253aa
lutianen <tianenlu957@gmail.com>
2023-04-13 15:19:36 +0800

    JZ17 打印从1到最大的n位数 思路：回溯 【for循环横向遍历，递归纵向遍历，回溯不断调整结果集】         - 用递归控制for循环嵌套的数量         - for 循环选择本层节点         - 优化回溯算法只有剪枝一种方法

--- JZOffer/JZ17.cc
+++ JZOffer/JZ17.cc
@@ -6,72 +6,71 @@
         用返回一个整数列表来代替打印
         n 为正整数
 
-    思路：回溯算法
+    思路：回溯 【for循环横向遍历，递归纵向遍历，回溯不断调整结果集】
+        - 用递归控制for循环嵌套的数量
+        - for 循环选择本层节点
+        - 优化回溯算法只有剪枝一种方法
  */
 
-#include <vector>
 #include <iostream>
 #include <string>
+#include <vector>
 
-using std::vector;
 using std::string;
+using std::vector;
 
 class Solution {
 public:
-    vector<int> printNumbers(int n) {
+    vector<int> printNumbers2(int n) {
         if (n <= 0) return {};
 
-        vector<string> temp;
-        string cur;
-        for(int i = 1; i <= n; ++i ){
-            DFS(temp, cur, 0, i);
+        vector<string> res;
+        string path;
+        for (int len = 1; len <= n; ++len) {
+            dfs(0, len, path, res);
         }
 
-        vector<int> r;
-        for(auto e : temp) {
-            r.push_back(std::stoi(e));
+        vector<int> nums;
+        for (const auto& str : res) {
+            nums.push_back(std::stoi(str));
         }
-
-        return r;
+        return nums;
     }
 
 private:
+    const char NUMBERS[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
     /**
-     * @brief 回溯算法
-     * 
      * @param res 结果
-     * @param cur 当前值
+     * @param path 当前值
      * @param x 当前值的第 x 位
      * @param len 当前值的总位数
      */
-    void DFS(vector<string>& res, string& cur, int x, int len) {
+    void dfs(int x, int len, string& path, vector<string>& res) {
         if (x == len) {
-            res.push_back(cur);
+            res.push_back(path);
             return;
         }
 
-        // 首位起始值：第 0 位则从 1 开始，避免出现 0 开头的数字字符串
-        int start = (x == 0) ? 1 : 0;
-
-        // 回溯
-        for(int i = start; i < 10; ++i) {
-            cur.push_back(NUMBERS[i]);
-            DFS(res, cur, x + 1, len);
-            cur.pop_back();
+        int st = x == 0 ? 1 : 0;
+        // for (选择：本层集合中元素（树中节点孩子的数量就是集合的大小）)
+        for (int i = st; i <= 9; i++) {
+            path.push_back(NUMBERS[i]);
+            dfs(x + 1, len, path, res);
+            path.pop_back();
         }
     }
-
-    const char NUMBERS[10] = {'0', '1', '2', '3', '4',
-                     '5', '6', '7', '8', '9'};
 };
 
 int main() {
     Solution s;
-    vector<int> ret = s.printNumbers(3);
+    // vector<int> ret = s.printNumbers(3);
+    vector<int> ret = s.printNumbers2(3);
     // for (auto iter = ret.begin(); iter != ret.end(); ++iter){
     //     std::cout << *iter << " ";
 
-    for(auto& v : ret)
-        std::cout << v << " ";
-    std::cout << "\n------------" << std::endl;
+    // for (auto& v : ret) std::cout << v << " ";
+    // std::cout << std::endl;
+
+    for(const int val : ret) printf("%d ", val);
 }
