1bb15c40b6337ae0073a813af1fc90dd076bcd08
lutianen <tianenlu957@gmail.com>
2023-04-14 20:16:26 +0800

    JZ24 反转链表 思路：     1. 利用几个指针     2. 利用栈先进后出的特性实现

--- JZOffer/JZ24.cc
+++ JZOffer/JZ24.cc
@@ -2,69 +2,64 @@
  * @file JZ24 反转链表
  * @brief 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
 
-    思路：迭代 或 利用栈的特性
+    思路：
+        1. 利用几个指针
+        2. 利用栈先进后出的特性实现
  */
 
-
 #include <cstdio>
+#include <stack>
+
+using std::stack;
 
 /// Definition for singly-linked list.
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-/*
-    #include <stack>
-    class Solution {
-    public:
-        // * @brief 利用 stack 的特性，先将链表遍历一遍并装入，在将栈中节点取出
-        // *  @note 最后一个节点的 next 不要忘记设置为 nullptr 
-        // * @param head 
-        // * @return ListNode*
-        ListNode* reverseList(ListNode* head) {
-            if(head == nullptr) 
-                return nullptr;
-            
-            ListNode* curr = head;
-            std::stack<ListNode*> temp {};
-
-            while(curr != nullptr) {
-                temp.push(curr);
-                curr = curr->next;
-            }
-
-            curr = head = temp.top();
-            temp.pop();
-            while(!temp.empty()) {
-                curr->next = temp.top();
-                temp.pop();
-                curr = curr->next;
-            }
-
-            // 坑点：最会一个节点的下一个节点为 nullptr
-            curr->next = nullptr;
-
-            return head;
-        }  
-    };
-*/
-
 class Solution {
 public:
+    // 3 根指针
     ListNode* reverseList(ListNode* head) {
         if (head == nullptr) return head;
 
-        ListNode* prev = nullptr;
-        ListNode* cur = head;
+        ListNode *pre = nullptr, *cur = head;
         while (cur != nullptr) {
-            ListNode* next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
+            ListNode* nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
         }
 
-        return prev;
+        return pre;
+    }
+
+    // 利用 stack 的特性
+    ListNode* reverseList2(ListNode* head) {
+        if (head == nullptr) return head;
+
+        // 先将链表入栈
+        stack<ListNode*> stk;
+        ListNode* cur = head;
+        while (cur != nullptr) {
+            stk.push(cur);
+            cur = cur->next;
+        }
+
+        // 出栈实现链表反转
+        head = stk.top();
+        cur = head;
+        stk.pop();
+        while (!stk.empty()) {
+            cur->next = stk.top();
+            cur = cur->next;
+            stk.pop();
+        }
+
+        // 最后一个节点的 next 置空
+        cur->next = nullptr;
+        return head;
     }
 };
