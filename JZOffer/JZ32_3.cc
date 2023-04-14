08a46defd59e9696f2a8bc385b28f5b8686cd733
lutianen <tianenlu957@gmail.com>
2023-04-14 21:51:24 +0800

    JZ32_3 从上到下打印二叉树 III 思路：BFS （队列） + reverse

--- JZOffer/JZ32_3.cc
+++ JZOffer/JZ32_3.cc
@@ -1,58 +1,55 @@
 /**
  * @file JZ32_3 从上到下打印二叉树 III
- * @brief 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，
+ * @brief
+ 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，
     第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
 
     思路：BFS （队列）
  */
 
+#include <algorithm>
 #include <cstdio>
 #include <queue>
 #include <vector>
-#include <deque>
-#include <algorithm>
 
-using std::vector;
 using std::queue;
+using std::vector;
 
 // Definition for a binary tree node.
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> res {};
-        if (root == nullptr) return res;
+    vector<vector<int>> levelOrder2(TreeNode* root) {
+        if (nullptr == root) return {};
 
-        bool LR = false;
-        queue<TreeNode*> que;
-        que.push(root);
-        while (!que.empty()) {
-            int k = que.size();
-            vector<int> temp;
-            while (k > 0) {
-                TreeNode* cur = que.front();
-                que.pop();
+        vector<vector<int>> ret{};
+        queue<TreeNode*> q{};
+        q.push(root);
+
+        bool L2R = false;
+        while (!q.empty()) {
+            int k = q.size();
+            vector<int> temp{};
+            for (int i = 0; i < k; i++) {
+                TreeNode* cur = q.front();
+                q.pop();
                 temp.push_back(cur->val);
-                k--;
-                
-                // 将子节点放入队列
-                if (cur->left) que.push(cur->left);
-                if (cur->right) que.push(cur->right);
+
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
             }
-            if (LR) {
-                std::reverse(temp.begin(), temp.end());
-                LR = false;
-            } else LR = true;
-            res.push_back(temp);
-            temp.clear();
+
+            if (L2R) reverse(temp.begin(), temp.end());
+            ret.push_back(temp);
+            L2R = !L2R;
         }
 
-        return res;
+        return ret;
     }
 };
