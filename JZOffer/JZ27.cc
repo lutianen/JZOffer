25581e0f40a4da6d2560c3346cdb44fa82b1566f
lutianen <tianenlu957@gmail.com>
2023-04-14 21:02:58 +0800

    JZ27 二叉树的镜像 思路：         1. 递归 - 可能出现栈溢出的情况         2：利用辅助栈，手动维护调用栈

--- JZOffer/JZ27.cc
+++ JZOffer/JZ27.cc
@@ -2,13 +2,12 @@
  * @file JZ27 二叉树的镜像
  * @brief 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
 
-    思路：递归 + 先序遍历
-        交换左右节点即可
-
-    思路2：利用 栈 或 队列
+    思路：
+        1. 递归 - 可能出现栈溢出的情况
+        2：利用辅助栈，手动维护调用栈
  */
-#include <cstdio>
 #include <algorithm>
+#include <cstdio>
 #include <stack>
 
 using std::stack;
@@ -17,22 +16,18 @@ using std::swap;
 // Definition for a binary tree node.
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
     /**
-     * @brief 递归实现
-     *  可能会出现栈溢出的情况 
-     * @param root 
-     * @return TreeNode* 
+     * @brief 递归 - 可能会出现栈溢出的情况
      */
     TreeNode* mirrorTree(TreeNode* root) {
-        if (root == nullptr)
-            return root;
+        if (root == nullptr) return root;
 
         // 节点交换
         std::swap(root->left, root->right);
@@ -45,26 +40,27 @@ public:
     }
 
     /**
-     * @brief 非递归实现 - 辅助栈
-     * 
-     * @param root 
-     * @return TreeNode* 
+     * @brief 非递归实现 - 辅助栈(手动维护递归栈)
      */
     TreeNode* mirrorTree2(TreeNode* root) {
-        if (root == nullptr)
-            return root;
+        if (root == nullptr) return root;
 
         stack<TreeNode*> stk;
+
+        // 入栈，相当于递归第一次调用
         stk.push(root);
-        
-        while(!stk.empty()) {
+
+        while (!stk.empty()) {
+            // 出栈
             TreeNode* curr = stk.top();
             stk.pop();
 
+            // 节点交换
+            swap(curr->left, curr->right);
+
+            // 左右子节点入栈，相当于递归调用
             if (curr->left != nullptr) stk.push(curr->left);
             if (curr->right != nullptr) stk.push(curr->right);
-
-            swap(curr->left, curr->right);
         }
 
         return root;
