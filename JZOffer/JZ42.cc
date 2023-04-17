c7f0a388d0ff13fcf95e2f5c8e3c78241b801ffb
lutianen <tianenlu957@gmail.com>
2023-04-17 10:38:06 +0800

    JZ42 连续子数组的最大和 1. 动态规划 maxSubArray         - dp[i] 表示以 i 位置为结尾时，子数组和的最大值         -  dp[i] = max(num[i], dp[i - 1] + nums[i]) 2. 贪心算法

--- JZOffer/JZ42.cc
+++ JZOffer/JZ42.cc
@@ -1,38 +1,39 @@
 /**
  * @file JZ42 连续子数组的最大和
- * @brief 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ * @brief
+ 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
     要求时间复杂度为O(n)。
- * 
+ *
     思路：
         1. 动态规划 maxSubArray
             - dp[i] 表示以 i 位置为结尾时，子数组和的最大值
             dp[i] = max(num[i], dp[i - 1] + nums[i])
 
-        2. 贪心算法 maxSubArray1
+        2. 贪心算法
  */
 
-#include <vector>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <vector>
 
-using std::vector;
 using std::max;
+using std::vector;
 
 class Solution {
 public:
+    // 动态规划
     int maxSubArray(vector<int>& nums) {
         vector<int> dp(nums.size());
         dp[0] = nums[0];
-        int m = dp[0];
+
         for (int i = 1; i < nums.size(); ++i) {
             dp[i] = max(nums[i], dp[i - 1] + nums[i]);
-            
-            m = max(dp[i], m);
         }
 
-        return m;
+        return *max_element(dp.begin(), dp.end());
     }
 
+    // 贪心
     int maxSubArray1(vector<int>& nums) {
         int greatestSum = 0x80000000;
         int currSum = 0;
@@ -43,10 +44,10 @@ public:
             } else {
                 currSum += nums[i];
             }
-            
+
             if (greatestSum < currSum) {
                 greatestSum = currSum;
-            } 
+            }
         }
 
         return greatestSum;
@@ -54,8 +55,8 @@ public:
 };
 
 int main() {
-   Solution s;
-   vector<int> arr {2, 3, -1, 2, -5, 4};
-   int ret = s.maxSubArray(arr);
-   std::cout << ret << std::endl;
+    Solution s;
+    vector<int> arr{2, 3, -1, 2, -5, 4};
+    int ret = s.maxSubArray(arr);
+    std::cout << ret << std::endl;
 }
