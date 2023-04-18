deba64a02d9ac7f492c62339f3dead858f8f180a
lutianen <tianenlu957@gmail.com>
2023-04-18 17:45:23 +0800

    JZ61 扑克牌中的顺子

--- JZOffer/JZ61.cc
+++ JZOffer/JZ61.cc
@@ -1,53 +1,72 @@
 /**
  * @file JZ61 扑克牌中的顺子
- * @brief 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。
-    2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14
+ * @brief
+        从若干副扑克牌中随机抽5张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+        2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为0,可以看成任意数字。A不能视为14
 
     思路：假设最小值为 x，则正常情况下最大值为 x + 4，它们的差值 D = 4 < 5；
          另外，正常情况下，不能出现重复数字
  */
 
-#include <vector>
-#include <set>
+#include <algorithm>
 #include <climits>
+#include <iostream>
+#include <set>
+#include <vector>
 
-using std::vector;
 using std::set;
+using std::vector;
 
 class Solution {
 public:
     bool isStraight(vector<int>& nums) {
-        int max = INT_MIN, min = INT_MAX;
-        
-        // 利用 set 
-        set<int> s;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) continue;
-            
-            // 记录最值
-            max = max > nums[i] ? max : nums[i];
-            min = min < nums[i] ? min : nums[i];
+        int zeroNum = 0, maxV = INT_MIN, minV = INT_MAX;
+        set<int> st;
+        for (int& e : nums) {
+            if (e != 0) {
+                // maxV = std::max(maxV, e);
+                // minV = std::min(minV, e);
 
-            if (s.find(nums[i]) == s.end()) {
-                s.insert(nums[i]);
-            } else return false;
+                maxV = maxV > e ? maxV : e;
+                minV = minV < e ? minV : e;
+                st.insert(e);
+            } else
+                zeroNum++;
         }
 
-        return max - min < 5;
+        // if (maxV - minV >= 5 || (st.size() + zeroNum != 5)) return false;
+        // return true;
+
+        return (maxV - minV < nums.size()) &&
+               (st.size() + zeroNum == nums.size());
     }
-    
-    // 相比于方法 1，利用了 set 的自身有序性（底层容器为红黑树），则不用刻意利用变量记录最值
+
+    // 相比于方法 1，利用了 set
+    // 的自身有序性（底层容器为红黑树），则不用刻意利用变量记录最值
     bool isStraight2(vector<int>& nums) {
-        // 利用 set 
+        // 利用 set
         set<int> s;
         for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) continue;
-
-            if (s.find(nums[i]) == s.end()) {
-                s.insert(nums[i]);
-            } else return false;
+            if (nums[i] != 0) {
+                if (s.find(nums[i]) == s.end())
+                    s.insert(nums[i]);
+                else
+                    return false;
+            }
         }
-
         return *s.rbegin() - *s.begin() < 5;
     }
 };
+
+int main(int argc, char* argv[]) {
+    int k;
+    std::cin >> k;
+    vector<int> nums(k);
+
+    for (int i = 0; i < k; i++) {
+        std::cin >> nums[i];
+    }
+
+    Solution so;
+    std::cout << so.isStraight(nums) << std::endl;
+}
