0f1a2b55ddea7434546a9defe0cb8c7806154324
lutianen <tianenlu957@gmail.com>
2023-04-26 22:11:40 +0800

    LQ19 删除链表的倒数第 N 个结点 思路：     双指针，快指针先走 n 步，然后再同时走到链表最后     @note 需要一个哑节点 dummyNode

---
+++ LQ/LQ19.cc
@@ -0,0 +1,43 @@
+/**
+ * @file LQ19 删除链表的倒数第 N 个结点
+ * @brief 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+    思路：双指针，快指针先走 n 步，然后再同时走到链表最后
+    @note 需要一个哑节点 dummyNode
+ */
+
+//  Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    // 思路：双指针，快指针先走 n 步，然后再同时走到链表最后
+    // @note 需要一个哑节点 dummyNode
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (n < 0) return nullptr;
+
+        ListNode* fast = head;
+        for (int i = n; i > 0; --i) {
+            fast = fast->next;
+        }
+
+        ListNode* dummpNode = new ListNode(-1, head);
+        ListNode* curr = dummpNode;
+        while (fast != nullptr) {
+            fast = fast->next;
+            curr = curr->next;
+        }
+
+        curr->next = curr->next->next;
+        ListNode* ret = dummpNode->next;
+        delete dummpNode;
+
+        return ret;
+    }
+};
