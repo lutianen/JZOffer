fceb6a7f02fff614d21918188f3d09477a0cfc3c
lutianen <tianenlu957@gmail.com>
2023-05-22 20:05:48 +0800

    CodeTop: 17. 二叉树的锯齿形层序遍历

---
+++ CodeTop/CodeTop017.cc
@@ -0,0 +1,63 @@
+/**
+ * @brief 二叉树的锯齿形层序遍历
+ * @link https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using std::reverse;
+using std::queue;
+using std::vector;
+using std::cout;
+using std::endl;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(nullptr == root) return {};
+
+        vector<vector<int>> res {};
+        bool L2R = true;
+        queue<TreeNode *> que;
+        que.push(root);
+        while(!que.empty()) {
+            int k = que.size();
+            vector<int> temp{};
+            for (int i = 0; i < k; ++i) {
+                TreeNode *cur = que.front();
+                que.pop();
+                temp.push_back(cur->val);
+
+                if (cur->left != nullptr) que.push(cur->left);
+                if (cur->right != nullptr) que.push(cur->right);
+            }
+
+            if (!L2R)
+                reverse(temp.begin(), temp.end());
+            
+            res.push_back(temp);
+            L2R = !L2R;
+        }
+
+        return res;
+    }
+};
+
+int main () {
+    cout << ":)" << endl;
+}
--- CodeTop/README.md
+++ CodeTop/README.md
@@ -464,3 +464,44 @@
         return cnt;
     }
     ```
+
+17. 二叉树的锯齿形层序遍历
+
+    ```c++
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(nullptr == root) return {};
+    
+        vector<vector<int>> res {};
+        bool L2R = true;
+        queue<TreeNode *> que;
+        que.push(root);
+        while(!que.empty()) {
+            int k = que.size();
+            vector<int> temp{};
+            for (int i = 0; i < k; ++i) {
+                TreeNode *cur = que.front();
+                que.pop();
+                temp.push_back(cur->val);
+    
+                if (cur->left != nullptr) que.push(cur->left);
+                if (cur->right != nullptr) que.push(cur->right);
+            }
+    
+            if (!L2R)
+                reverse(temp.begin(), temp.end());
+    
+            res.push_back(temp);
+            L2R = !L2R;
+        }
+    
+        return res;
+    }
+    ```
