aace9d17131b11ab1157d86aa8a5412395c78ca2
lutianen <tianenlu957@gmail.com>
2023-05-24 21:09:53 +0800

    CodeTop: 27. 重排链表

---
+++ CodeTop/CodeTop027.cc
@@ -0,0 +1,55 @@
+/**
+ * @brief 重排链表
+ * @link https://leetcode.cn/problems/reorder-list/
+ */
+
+#include <iostream>
+#include <deque>
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (head == nullptr) return;
+
+        std::deque<ListNode*> dq;
+        ListNode *cur = head;
+
+        while(nullptr != cur) {
+            dq.push_back(cur);
+            cur = cur->next;
+        }
+
+        bool flagFront = false;
+        cur = dq.front();
+        dq.pop_front();
+        while(!dq.empty()) {
+            if (flagFront) {
+                cur->next = dq.front();
+                dq.pop_front();
+            } else {
+                cur->next = dq.back();
+                dq.pop_back();
+            }
+
+            flagFront = !flagFront;
+            cur = cur->next;
+        }
+        cur->next = nullptr;
+    }
+};
+
+int main () {
+    std::cout << " list .. " << std::endl;
+    return 0;
+}
--- CodeTop/README.md
+++ CodeTop/README.md
@@ -854,3 +854,44 @@
         return sum;
     }
     ```
+
+27. 重排链表
+
+    ```c++
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    void reorderList(ListNode* head) {
+        if (head == nullptr) return;
+
+        std::deque<ListNode*> dq;
+        ListNode *cur = head;
+
+        while(nullptr != cur) {
+            dq.push_back(cur);
+            cur = cur->next;
+        }
+
+        bool flagFront = false;
+        cur = dq.front();
+        dq.pop_front();
+        while(!dq.empty()) {
+            if (flagFront) {
+                cur->next = dq.front();
+                dq.pop_front();
+            } else {
+                cur->next = dq.back();
+                dq.pop_back();
+            }
+
+            flagFront = !flagFront;
+            cur = cur->next;
+        }
+        cur->next = nullptr;
+    }
+    ```
