a2a94e786bbe63699fde604367c2f01337bc7f39
lutianen <tianen.xd@gmail.com>
2023-09-25 17:13:51 +0800

    Update smart_pointer.drawio

--- Utils/test.cc
+++ Utils/test.cc
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <pthread.h>
+
+std::vector<int> func(int a) {
+    std::cout << __FUNCTION__ << std::endl;
+    return {};
+}
 
 int main() {
     {
@@ -19,5 +25,27 @@ int main() {
         std::cout << __LINE__ << ": ";
         for (const auto& e : vec_a) std::cout << e << " ";
         std::cout << std::endl;
+    }
+    {
+        int i = 0;
+        // ++i 是左值
+        ++i = 10;
+
+        // "01234" 是左值，且类型为 const char [6]
+        const char(&left)[6] = "01234";
+
+        /*
+            decltype(expr) 在 expr 为左值且非无括号包裹的 id
+           表达式与类成员表达式时，会返回左值引用
+        */
+        static_assert(std::is_same<decltype("01234"), const char(&)[6]>::value,
+                      "");
+        static_assert(std::is_same<decltype("01234"), decltype(left)>::value,
+                      "");
+        // const char(&&right)[6] = "01234"; // "01234" 是左值，不可被 右值引用
+    }
+    {
+        func(1);
+        
     }
 }
